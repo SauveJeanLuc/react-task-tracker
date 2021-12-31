c525f4198036720f4ae3ed2a47db23fefbe08999
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-12-31 10:19:04 +0200

    Constants in JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 function App() {
+  const name = "sauve";
+  const x = true;
   return (
     <div className="App">
       <h1>
         Hello From React
       </h1>
-      <h2> Hello </h2>
+      <h2> Hello {name} </h2>
+      <h2> Hello {x ? 'Yes' : 'No' } </h2>
     </div>
   );
 }
