391966ae671bdc22343cd130b7dda836fa9800c6
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-01 23:51:57 +0200

    Set the Array in UseState

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,26 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 6th at 1:30pm',
-        reminder: false
-    },
-];
+import { useState } from 'react'
 const Tasks = () => {
+
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Meeting at School',
+            day: 'Feb 6th at 1:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'Food Shopping',
+            day: 'Feb 6th at 1:30pm',
+            reminder: false
+        },
+    ])
     return (
         <>
             {tasks.map((task) => (
