6bb72ec902992ef2b20ca4de8230fc4abfa69112
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-01 23:55:38 +0200

    Have UseState in App.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,6 @@
-import { useState } from 'react'
-const Tasks = () => {
+const Tasks = ({tasks}) => {
 
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true
-        },
-        {
-            id: 2,
-            text: 'Meeting at School',
-            day: 'Feb 6th at 1:30pm',
-            reminder: true
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Feb 6th at 1:30pm',
-            reminder: false
-        },
-    ])
+    
     return (
         <>
             {tasks.map((task) => (
