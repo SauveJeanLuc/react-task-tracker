8ce3c597e0c99595ebd397be94d56f276707731b
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-02 00:10:40 +0200

    add more elements on Task Component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,8 @@
-const Task = () => {
+const Task = ({ task }) => {
     return (
         <div className='task'>
-            <h3>My Task</h3>
+            <h3>{task.text}</h3>
+            <p>{task.day}</p>
         </div>
     )
 }
