53c65e78a3dadf70c7fa4c0eadd82da30025ac38
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-01 23:45:22 +0200

    Use of map to iterate through

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
   return (
     <div className="App">
-        <Header/>
+      <Header />
+      <Tasks />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -20,9 +20,11 @@ const tasks = [
 ];
 const Tasks = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            {tasks.map((task) => (
+                <h3>{task.text}</h3>
+            ))}
+        </>
     )
 }
 
