087360da68521be7abb21d2e9d60be06ea12e18a
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-03 18:52:44 +0200

    Add Reminder Paragraph

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,6 +3,7 @@ const Task = ({ task }) => {
         <div className='task'>
             <h3>{task.text}</h3>
             <p>{task.day}</p>
+            <p>{task.reminder == true ? 'Reminder set' : 'Reminder not set'}</p>
         </div>
     )
 }
