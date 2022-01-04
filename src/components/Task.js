965a5f7d205a4af9ba3f315428cda634b23fa340
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-04 14:01:53 +0200

    Used FaTimes as a react icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,11 @@
+import { FaTimes } from 'react-icons/fa'
+
 const Task = ({ task }) => {
     return (
         <div className='task'>
-            <h3>{task.text}</h3>
+            <h3>{task.text} <FaTimes style={ { color: 'red', cursor: 'pointer'} }/></h3>
             <p>{task.day}</p>
-            <p>{task.reminder == true ? 'Reminder set' : 'Reminder not set'}</p>
+            <p>{task.reminder === true ? 'Reminder set' : 'Reminder not set'}</p>
         </div>
     )
 }
