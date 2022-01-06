332390ff04ae08da45a47e2b84f4b2ca96a1bd69
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-06 23:07:39 +0200

    changed color

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa'
 const Task = ({ task, onDelete, onToggle}) => {
     return (
         <div className='task' onDoubleClick={() => onToggle(task.id)}> 
-            <h3>{task.text} <FaTimes style={ { color: 'red', cursor: 'pointer'} } onClick={ () => onDelete(task.id)}/></h3>
+            <h3>{task.text} <FaTimes style={ { color: 'black', cursor: 'pointer'} } onClick={ () => onDelete(task.id)}/></h3>
             <p>{task.day}</p>
             <p>{task.reminder === true ? 'Remind Me' : 'Do not Remind me'}</p>
         </div>
