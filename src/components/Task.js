c0b15072bcc07a464d6cdce61949e5d10f615df0
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-07 15:18:36 +0200

    Set Reminder to be Green-bordered

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle}) => {
     return (
-        <div className='task' onDoubleClick={() => onToggle(task.id)}> 
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}> 
             <h3>{task.text} <FaTimes style={ { color: 'black', cursor: 'pointer'} } onClick={ () => onDelete(task.id)}/></h3>
             <p>{task.day}</p>
             <p>{task.reminder === true ? 'Remind Me' : 'Do not Remind me'}</p>
