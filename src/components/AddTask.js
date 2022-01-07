813d43778e6cef625dede1b1ebbc52c4a5c966c5
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-07 15:36:11 +0200

    Created AddTask Component

---
+++ src/components/AddTask.js
@@ -0,0 +1,24 @@
+const AddTask = () => {
+    return (
+        <form className="add-form" action="">
+            <div className="form-control">
+                <label>Task</label>
+                <input type="text" placeholder="Add Task"/>
+            </div>
+            
+            <div className="form-control">
+                <label>Day & Time</label>
+                <input type="text" placeholder="Add Day & Time"/>
+            </div>
+
+            <div className="form-control form-control-check">
+                <label>Set Reminder</label>
+                <input type="checkbox" placeholder="Add Task"/>
+            </div>
+
+            <input type="submit" value='Save Task'/>
+        </form>
+    )
+}
+
+export default AddTask
