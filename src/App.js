710169ece3f4515b47d4e08cf44af258142f4ee4
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-07 21:39:51 +0200

    AddTask function in App.js

--- src/App.js
+++ src/App.js
@@ -26,6 +26,11 @@ function App() {
       },
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    console.log(task)
+  } 
+
   // Delete Task  
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -39,7 +44,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
       ) : (
