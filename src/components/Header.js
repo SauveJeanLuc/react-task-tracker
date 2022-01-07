8edba1d8df6678f4e62164b4bad84049c44a6898
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-07 21:59:26 +0200

    onAdd use in Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,11 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-
-    const onClick = (e) => {
-        console.log(e)
-    }
-    
+const Header = ({ title, onAdd}) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button color='green' text='Add' onClick={onAdd}/>
         </header>
     )
 }
