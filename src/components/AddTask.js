922233638bd4b04e7ea927a154c6ae38abe91352
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-07 21:40:37 +0200

    onSubmit event in form tag

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,27 @@
 import { useState } from 'react';
 
-const AddTask = () => {
+const AddTask = ( { onAdd } ) => {
 
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!text) {
+            alert('Please enter a text')
+            return
+        }
+
+        onAdd({ text, day, reminder })
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
     return (
-        <form className="add-form" action="">
+        <form className="add-form" onSubmit={onSubmit}>
             <div className="form-control">
                 <label>Task</label>
                 <input type="text" placeholder="Add Task" value={text} onChange={(e) => setText(e.target.value)}/>
