15308dce49e3c9ecb6d307307cc47ed5f9f0ffdd
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-08 21:59:31 +0200

    pass showAddTask to Header

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask}/>
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
