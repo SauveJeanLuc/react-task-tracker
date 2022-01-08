30ac53bfc3a1594275921961c883d24c82d68dc6
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-08 22:05:13 +0200

    Make color dynamic

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ title, onAdd, showAdd}) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
     )
 }
