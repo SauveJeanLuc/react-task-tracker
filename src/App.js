760133ab59b4a6d55aca773bc4b89354e45f59b0
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-09 21:22:24 +0200

    included useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AddTask from './components/AddTask';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
