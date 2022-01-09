4f0164ae70357d5e7ade5f8ead6b860457c96494
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-09 22:18:49 +0200

    Change FaTimes' component's style color to red

--- src/App.js
+++ src/App.js
@@ -28,11 +28,21 @@ function App() {
   }
   
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = { id, ...task } 
-    setTasks([...tasks, newTask])
-    console.log(id)
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:500/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task } 
+    // setTasks([...tasks, newTask])
   } 
 
   // Delete Task  
