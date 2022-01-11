db39dcd03d613d1537ea09abae28d2c0bb6ec876
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2022-01-11 08:18:24 +0200

    removed remind me paragraph

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,7 +5,7 @@ const Task = ({ task, onDelete, onToggle}) => {
         <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}> 
             <h3>{task.text} <FaTimes style={ { color: 'red', cursor: 'pointer'} } onClick={ () => onDelete(task.id)}/></h3>
             <p>{task.day}</p>
-            <p>{task.reminder === true ? 'Remind Me' : 'Do not Remind me'}</p>
+            {/* <p>{task.reminder === true ? 'Remind Me' : 'Do not Remind me'}</p> */}
         </div>
     )
 }
